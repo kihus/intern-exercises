c709343ae092d4908619ad2be5acdb6c7c84e0fd
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-07 21:38:04 -0300

    feat: update MediaPonderada function to accept external input

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Exercises.Entities;
 
 // Média e Aprovação
+Console.WriteLine("----- Média e Aprovação -----");
 Console.WriteLine("Digite 3 notas do aluno(a): ");
 var primeiraNota = double.Parse(Console.ReadLine() ?? "0");
 var segundaNota = double.Parse(Console.ReadLine() ?? "0");
@@ -9,12 +10,14 @@ var terceiraNota = double.Parse(Console.ReadLine() ?? "0");
 Calculator.Media(primeiraNota, segundaNota, terceiraNota);
 
 // Verificação de número par ou inteiro
+Console.WriteLine("----- Verificação de número par ou inteiro -----");
 Console.Write("Coloque um número inteiro: ");
 var num = int.Parse(Console.ReadLine() ?? "0");
 
 Calculator.VeficaParOuImpar(num);
 
 // Maior de dois números
+Console.WriteLine("----- Maior de dois números -----");
 Console.WriteLine("Escreva dois números");
 var num1 = int.Parse(Console.ReadLine() ?? "0");
 var num2 = int.Parse(Console.ReadLine() ?? "0");
@@ -22,6 +25,7 @@ var num2 = int.Parse(Console.ReadLine() ?? "0");
 Calculator.MaiorNumero(num1, num2);
 
 // Calculo de IMC
+Console.WriteLine("----- Calculo de IMC -----");
 Console.Write("Escreva sua altura: ");
 var altura = double.Parse(Console.ReadLine() ?? "0");
 
@@ -31,6 +35,7 @@ var peso = double.Parse(Console.ReadLine() ?? "0");
 Calculator.CalcularIMC(altura, peso);
 
 // Verificação de múltiplos
+Console.WriteLine("----- Verificação de múltiplos ----- ");
 Console.WriteLine("Escreva 2 números inteiros");
 var numMult1 = int.Parse(Console.ReadLine() ?? "0");
 var numMult2 = int.Parse(Console.ReadLine() ?? "0");
@@ -38,6 +43,7 @@ var numMult2 = int.Parse(Console.ReadLine() ?? "0");
 Calculator.Multiplos(numMult1, numMult2);
 
 // Desconto em produto
+Console.WriteLine("----- Desconto em produto -----");
 Console.Write("Digite o valor do produto: ");
 var valorProd = double.Parse(Console.ReadLine() ?? "0");
 
@@ -47,6 +53,7 @@ var formaPagamento = int.Parse(Console.ReadLine() ?? "0");
 Calculator.Desconto(valorProd, formaPagamento);
 
 // Classificação de triangulo
+Console.WriteLine("----- Classificação de triangulo -----");
 Console.WriteLine("Escreva os 3 lados do triangulo");
 
 Console.Write("Primeiro lado: ");
@@ -61,13 +68,25 @@ var terceiroLado = double.Parse(Console.ReadLine() ?? "0");
 Calculator.ClassificarTriangulo(primeiroLado, segundoLado, terceiroLado);
 
 // Média ponderada
-var nota1 = 6;
-var peso1 = 20;
+Console.WriteLine("----- Média ponderada -----");
+Console.Write("Digite a primeira nota: ");
+var nota1 = double.Parse(Console.ReadLine() ?? "0");
 
-var nota2 = 5;
-var peso2 = 30;
+Console.Write("Digite o peso da nota: ");
+var peso1 = int.Parse(Console.ReadLine() ?? "0");
 
-var nota3 = 5;
-var peso3 = 50;
+Console.Write("Digite a segunda nota: ");
+var nota2 = double.Parse(Console.ReadLine() ?? "0");
 
-Calculator.MediaPonderada(nota1, peso1, nota2, peso2, nota3, peso3);
+Console.Write("Digite o peso da nota: ");
+var peso2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite a terceira nota: ");
+var nota3 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o peso da nota: ");
+var peso3 = int.Parse(Console.ReadLine() ?? "0");
+
+Calculator.MediaPonderada(nota1, peso1, 
+                          nota2, peso2, 
+                          nota3, peso3);
