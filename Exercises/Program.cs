c33cfff610e27c56e50f3309122fdc25d01fc3db
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-08 09:37:58 -0300

    refactor: add '!' in 'Console.ReadLine()' to ignore warning

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -5,18 +5,17 @@ try
 {
 
     // Média e Aprovação
-
     Console.WriteLine("----- Média e Aprovação -----");
     Console.WriteLine("Digite 3 notas do aluno(a): ");
 
     Console.Write("Primeira nota: ");
-    var primeiraNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var primeiraNota = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Segunda nota: ");
-    var segundaNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var segundaNota = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Terceira nota: ");
-    var terceiraNota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var terceiraNota = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Calculator.Media(primeiraNota, segundaNota, terceiraNota);
     Console.WriteLine();
@@ -24,7 +23,7 @@ try
     // Verificação de número par ou impar
     Console.WriteLine("----- Verificação de número par ou impar -----");
     Console.Write("Coloque um número inteiro: ");
-    var num = int.Parse(Console.ReadLine() ?? "0");
+    var num = int.Parse(Console.ReadLine()!);
 
     if (Calculator.VeficaParOuImpar(num))
         Console.WriteLine("Número par");
@@ -38,10 +37,10 @@ try
     Console.WriteLine("Escreva dois números");
 
     Console.Write("Primeiro número: ");
-    var num1 = int.Parse(Console.ReadLine());
+    var num1 = int.Parse(Console.ReadLine()!);
 
     Console.Write("Primeiro número: ");
-    var num2 = int.Parse(Console.ReadLine());
+    var num2 = int.Parse(Console.ReadLine()!);
 
     Calculator.MaiorNumero(num1, num2);
     Console.WriteLine();
@@ -49,10 +48,10 @@ try
     // Calculo de IMC
     Console.WriteLine("----- Calculo de IMC -----");
     Console.Write("Escreva sua altura: ");
-    var altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var altura = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Escreva seu peso: ");
-    var peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var peso = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Calculator.CalcularIMC(peso, altura);
     Console.WriteLine();
@@ -60,8 +59,8 @@ try
     // Verificação de múltiplos
     Console.WriteLine("----- Verificação de múltiplos ----- ");
     Console.WriteLine("Escreva 2 números inteiros");
-    var numMult1 = int.Parse(Console.ReadLine());
-    var numMult2 = int.Parse(Console.ReadLine());
+    var numMult1 = int.Parse(Console.ReadLine()!);
+    var numMult2 = int.Parse(Console.ReadLine()!);
 
     Calculator.Multiplos(numMult1, numMult2);
     Console.WriteLine();
@@ -69,10 +68,10 @@ try
     // Desconto em produto
     Console.WriteLine("----- Desconto em produto -----");
     Console.Write("Digite o valor do produto: ");
-    var valorProd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var valorProd = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Digite a forma de pagamento (1 - à vista, 2 - parcelado): ");
-    var formaPagamento = int.Parse(Console.ReadLine());
+    var formaPagamento = int.Parse(Console.ReadLine()!);
 
     Calculator.Desconto(valorProd, formaPagamento);
     Console.WriteLine();
@@ -82,13 +81,13 @@ try
     Console.WriteLine("Escreva os 3 lados do triangulo");
 
     Console.Write("Primeiro lado: ");
-    var primeiroLado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var primeiroLado = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Segundo lado: ");
-    var segundoLado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var segundoLado = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Terceiro lado: ");
-    var terceiroLado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var terceiroLado = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Calculator.ClassificarTriangulo(primeiroLado, segundoLado, terceiroLado);
     Console.WriteLine();
@@ -96,22 +95,22 @@ try
     // Média ponderada
     Console.WriteLine("----- Média ponderada -----");
     Console.Write("Digite a primeira nota: ");
-    var nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var nota1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Digite o peso da nota: ");
-    var peso1 = int.Parse(Console.ReadLine());
+    var peso1 = int.Parse(Console.ReadLine()!);
 
     Console.Write("Digite a segunda nota: ");
-    var nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var nota2 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Digite o peso da nota: ");
-    var peso2 = int.Parse(Console.ReadLine());
+    var peso2 = int.Parse(Console.ReadLine()!);
 
     Console.Write("Digite a terceira nota: ");
-    var nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    var nota3 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
     Console.Write("Digite o peso da nota: ");
-    var peso3 = int.Parse(Console.ReadLine());
+    var peso3 = int.Parse(Console.ReadLine()!);
 
     Calculator.MediaPonderada(nota1, peso1,
                               nota2, peso2,
